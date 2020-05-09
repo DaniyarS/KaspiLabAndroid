cd3df641080ccf723aa6ae78956bca22ce40ef36
daniyars <dslamkul14@gmail.com>
2020-05-09 17:16:48 +0600

    task 5a

---
+++ app/src/main/java/com/example/kaspicourse/MessageAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.kaspicourse
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MessageAdapter : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MessageAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(inflater: LayoutInflater, parent: ViewGroup) : RecyclerView.ViewHolder(inflater.inflate(R.layout.activity_main, parent, false)) {
+
+    }
+
+}
